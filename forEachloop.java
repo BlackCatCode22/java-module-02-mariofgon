44aff00169132c933683bc95641aee6e9a774214
mariofgon <0769210@my.scccd.edu>
2025-09-04 11:31:27 -0700

    Add files via upload

---
+++ forEachloop.java
@@ -0,0 +1,18 @@
+public class forEachloop {
+
+    public static void main(String[] args) {
+
+        String[] cars = {"GTR R35", "Durango SRT", "TRX"};
+
+        for (String car : cars) {
+            System.out.println(car);
+        }
+
+        for(int i=0; i<5; i++) {
+
+            System.out.println(i);
+        }
+
+
+    }
+}
